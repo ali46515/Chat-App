8e2f35fbb0963a9590710705b7e277a774b39093
ali46515 <aliarshad.9474@gmail.com>
2026-04-02 22:15:50 +0500

    Added basic msg and file inputs

--- frontend/src/components/ChatBox/ChatBox.jsx
+++ frontend/src/components/ChatBox/ChatBox.jsx
@@ -6,9 +6,17 @@ const ChatBox = () => {
     return (
         <div className="chat-box">
             <div className="chat-user">
-                <img src={ assets.profile_img} alt="" />
-                <p>Ahmad Ijaz <img src={assets.green_dot} alt="" /></p>
+                <img src={assets.profile_img} alt="" />
+                <p>Ahmad Ijaz <img src={assets.green_dot} className='dot' alt="" /></p>
                 <img src={assets.help_icon} className='help' alt="" />
+            </div>
+            <div className="chat-input">
+                <input type="text" placeholder='Send a message' />
+                <input type="file" id="image" accept='image/png, image/jpeg' hidden />
+                <label htmlFor="image">
+                    <img src={assets.gallery_icon} alt="" />
+                </label>
+                <img src={assets.send_button} alt="" />
             </div>
         </div>
     )
