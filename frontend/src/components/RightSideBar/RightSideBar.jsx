049f5502a7187ede8f50747e7356d76a6ac347fb
ali46515 <aliarshad.9474@gmail.com>
2026-04-03 21:54:45 +0500

    Implement styling on right sidebar

--- frontend/src/components/RightSideBar/RightSideBar.jsx
+++ frontend/src/components/RightSideBar/RightSideBar.jsx
@@ -10,6 +10,19 @@ const RightSideBar = () => {
                 <h3>Ahmad Ijaz <img src={assets.green_dot} alt="" className='dot' /></h3>
                 <p>Salam, Kesy ho?</p>
             </div>
+            <hr />
+            <div className="rs-media">
+                <p>Media</p>
+                <div>
+                    <img src={assets.pic1} alt="" />
+                    <img src={assets.pic2} alt="" />
+                    <img src={assets.pic3} alt="" />
+                    <img src={assets.pic4} alt="" />
+                    <img src={assets.pic1} alt="" />
+                    <img src={assets.pic2} alt="" />
+                </div>
+            </div>
+            <button>Logout</button>
         </div>
     )
 }
