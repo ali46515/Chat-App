f13b938c701093804453558574917caa2bcac5d0
ali46515 <aliarshad.9474@gmail.com>
2026-04-03 21:19:31 +0500

    Implement basic chat msgs

--- frontend/src/components/ChatBox/ChatBox.jsx
+++ frontend/src/components/ChatBox/ChatBox.jsx
@@ -10,6 +10,24 @@ const ChatBox = () => {
                 <p>Ahmad Ijaz <img src={assets.green_dot} className='dot' alt="" /></p>
                 <img src={assets.help_icon} className='help' alt="" />
             </div>
+
+            <div className="chat-msg">
+                <div className="s-msg">
+                    <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, sint minus. Esse iste, sunt suscipit temporibus delectus facilis ducimus qui. Perferendis libero excepturi officiis ab id! Fugit dolore at aliquid?</p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>11:30 AM</p>
+                    </div>
+                </div>
+                <div className="r-msg">
+                    <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, sint minus. Esse iste, sunt suscipit temporibus delectus facilis ducimus qui. Perferendis libero excepturi officiis ab id! Fugit dolore at aliquid?</p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>1:30 PM</p>
+                    </div>
+                </div>
+            </div>
+
             <div className="chat-input">
                 <input type="text" placeholder='Send a message' />
                 <input type="file" id="image" accept='image/png, image/jpeg' hidden />
