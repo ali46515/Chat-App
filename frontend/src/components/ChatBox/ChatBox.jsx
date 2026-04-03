9ebfb4a2d5df58e8bdde72106f0e036f7a0bd772
ali46515 <aliarshad.9474@gmail.com>
2026-04-03 21:39:17 +0500

    Implement simple right bar for profile

--- frontend/src/components/ChatBox/ChatBox.jsx
+++ frontend/src/components/ChatBox/ChatBox.jsx
@@ -19,6 +19,13 @@ const ChatBox = () => {
                         <p>11:30 AM</p>
                     </div>
                 </div>
+                <div className="s-msg">
+                    <img src={assets.pic1} alt="" className='msg-img'/>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>11:30 AM</p>
+                    </div>
+                </div>
                 <div className="r-msg">
                     <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing elit?</p>
                     <div>
--- frontend/src/components/RightSideBar/RightSideBar.jsx
+++ frontend/src/components/RightSideBar/RightSideBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './RightSideBar.css'
+import assets from '/src/assets/assets.js'
 
 const RightSideBar = () => {
     return (
-        <div>RightSideBar</div>
+        <div className="rs">
+            <div className="rs-profile">
+                <img src={assets.profile_img} alt="" />
+                <h3>Ahmad Ijaz <img src={assets.green_dot} alt="" className='dot' /></h3>
+                <p>Salam, Kesy ho?</p>
+            </div>
+        </div>
     )
 }
 
