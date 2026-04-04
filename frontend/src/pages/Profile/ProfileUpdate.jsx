0a8891c350d1351cf87670004639bf7ec35ffa3c
ali46515 <aliarshad.9474@gmail.com>
2026-04-04 21:19:34 +0500

    Implement basic profile page

--- frontend/src/pages/Profile/ProfileUpdate.jsx
+++ frontend/src/pages/Profile/ProfileUpdate.jsx
@@ -1,8 +1,26 @@
 import React from "react";
-import './ProfileUpdate.css'
+import "./ProfileUpdate.css";
+import assets from "/src/assets/assets.js"
 
 const ProfileUpdate = () => {
-    return <>We are here in Profile</>
-}
+    return (
+        <div className="profile">
+            <div className="profile-container">
+                <form>
+                    <h3>Profile Details</h3>
+                    <label htmlFor="avatar">
+                        <input type="file" id="avatar" accept=".png, .jpg, .jpeg" />
+                        <img src={assets.avatar_icon} alt="" />
+                        upload profile image
+                    </label>
+                    <input type="text" placeholder="Your Name" required />
+                    <textarea placeholder="Write profile bio" required></textarea>
+                    <button type="submit">Save</button>
+                </form>
+                <img src={assets.logo_icon} alt="" />
+            </div>
+        </div>
+    );
+};
 
-export default ProfileUpdate
+export default ProfileUpdate;
