473ee81d6a5c2f5b53a0ddc597345aa4a88ec6c0
ali46515 <aliarshad.9474@gmail.com>
2026-04-04 21:34:04 +0500

    Implement state management for image upload

--- frontend/src/pages/Profile/ProfileUpdate.jsx
+++ frontend/src/pages/Profile/ProfileUpdate.jsx
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProfileUpdate.css";
 import assets from "/src/assets/assets.js"
 
 const ProfileUpdate = () => {
+    const [img, setImg] = useState(false)
+
     return (
         <div className="profile">
             <div className="profile-container">
                 <form>
                     <h3>Profile Details</h3>
                     <label htmlFor="avatar">
-                        <input type="file" id="avatar" accept=".png, .jpg, .jpeg" />
-                        <img src={assets.avatar_icon} alt="" />
+                        <input type="file" id="avatar" onChange={(e) => setImg(e.target.files[0])} accept=".png, .jpg, .jpeg" hidden />
+                        <img src={img ? URL.createObjectURL(img) : assets.avatar_icon} alt="" />
                         upload profile image
                     </label>
                     <input type="text" placeholder="Your Name" required />
                     <textarea placeholder="Write profile bio" required></textarea>
                     <button type="submit">Save</button>
                 </form>
-                <img src={assets.logo_icon} alt="" className="profile-pic" />
+                <img src={img ? URL.createObjectURL(img) : assets.logo_icon} alt="" className="profile-pic" />
             </div>
         </div>
     );
