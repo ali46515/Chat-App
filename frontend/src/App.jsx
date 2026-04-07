044843e71565e62503ce74f57f377a0e0aca5f42
ali46515 <aliarshad.9474@gmail.com>
2026-04-07 20:52:39 +0500

    Update app.jsx by implementing ToastContainer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,20 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/auth/Login.jsx'
 import Chat from "./pages/chat/Chat.jsx"
 import ProfileUpdate from "./pages/Profile/ProfileUpdate.jsx"
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/ReactToastify.css'
 
 
 function App() {
 
   return (<>
-    <Routes>
-      <Route path='/' element={<Login />}></Route>
-      <Route path='/chat' element={<Chat />}></Route>
-      <Route path='/profile' element={<ProfileUpdate />}></Route>
-
-    </Routes>
+    <ToastContainer>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/chat' element={<Chat />}></Route>
+        <Route path='/profile' element={<ProfileUpdate />}></Route>
+      </Routes>
+    </ToastContainer>
   </>)
 }
 
