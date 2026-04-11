89ade382559a74bbebffc21ed7589049fcb11abc
ali46515 <aliarshad.9474@gmail.com>
2026-04-11 21:17:13 +0500

    Connect socket with the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import http from "http";
 import app from "./app.js";
 import { config } from "dotenv";
+import { initSocket } from "./modules/WebSocket/socket.js";
 config();
 
 const server = http.createServer(app);
+
+initSocket(server);
 
 server.listen(process.env.PORT || 8080, () => {
   console.log(`Server running: http://localhost:${process.env.PORT}`);
