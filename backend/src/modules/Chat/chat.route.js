d59fde799e5c912209a42a57dc55ade921ccee13
ali46515 <aliarshad.9474@gmail.com>
2026-04-12 20:22:03 +0500

    Update chat routes

--- backend/src/modules/Chat/chat.route.js
+++ backend/src/modules/Chat/chat.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import * as chatController from "./chat.controller.js";
 import { auth } from "../../middleware/auth.middleware.js";
+import * as chatController from "./chat.controller.js";
 
 const router = express.Router();
 
-router.get("/:conversationId", auth, chatController.getMessages);
-router.patch("/read/:conversationId", auth, chatController.markRead);
+router.use(auth);
+
+router.get("/conversations", chatController.getConversations);
+router.get("/messages/:conversationId", chatController.getMessages);
+router.patch("/read/:conversationId", chatController.markRead);
 
 export default router;
